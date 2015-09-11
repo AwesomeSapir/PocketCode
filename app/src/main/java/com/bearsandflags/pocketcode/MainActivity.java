40b229550df91d7227614c35a934dcdd224b6b57
Sapir Melchin <mcnetfighter@gmail.com>
2015-09-11 14:48:02 +0300

    test change

--- app/src/main/java/com/bearsandflags/pocketcode/MainActivity.java
+++ app/src/main/java/com/bearsandflags/pocketcode/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends Activity {
         Context context=getApplicationContext();
 
         File noteDir=context.getDir("mydir", Context.MODE_PRIVATE);
+        //change
 
         //--------------------------------------------------------
 
